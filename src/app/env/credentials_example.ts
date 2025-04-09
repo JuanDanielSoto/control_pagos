f2f3caaff5a9b9d369edd6657c6fe0ed122bb122
unknown <halogeno321@gmail.com>
2025-04-08 20:03:49 -0600

    Añadido credenciales de ejemplo sin secretos

---
+++ src/app/env/credentials_example.ts
@@ -0,0 +1,4 @@
+export const env = {
+    GOOGLE_API_TOKEN:"busca en https://console.cloud.google.com/apis/credentials?authuser=1&hl=es-419&invt=AbuOqQ&project=deudores-453302",
+    GOOGLE_CLIENT_ID:"coloca aqui las credenciales en un archivo credentials.ts"
+} 
