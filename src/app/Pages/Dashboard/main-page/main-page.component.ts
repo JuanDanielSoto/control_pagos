18f79dce694feed28cd8840d24c3e2f1b4990d6a
unknown <halogeno321@gmail.com>
2025-04-08 23:23:01 -0600

    Tabla añadida

--- src/app/Pages/Dashboard/main-page/main-page.component.ts
+++ src/app/Pages/Dashboard/main-page/main-page.component.ts
@@ -8,13 +8,27 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+  mesSeleccionado: string = '';
   data = [
-    { name: 'Enero', value: 5000 },
-    { name: 'Febrero', value: 3200 },
-    { name: 'Marzo', value: 2800 }
+    { name: 'Miguel CEO', value: 12000 },
+    { name: 'Joss', value: 7000 },
+    { name: 'Daniel', value: 5000 }
   ];
+
+  formatCurrency(value: number): string {
+    return new Intl.NumberFormat('es-MX', {
+      style: 'currency',
+      currency: 'MXN',
+      minimumFractionDigits: 0,
+    }).format(value);
+  }
 
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
+
+  dataFiltrada() {
+    if (!this.mesSeleccionado) return this.data;
+    return this.data.filter(item => item.name === this.mesSeleccionado);
+  }
 }
